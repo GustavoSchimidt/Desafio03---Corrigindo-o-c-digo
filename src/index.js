bce3fe8ff89653159107d2676778b32720957713
Gustavo Schimidt <schmidtgustavo7@gmail.com>
2021-07-07 11:27:12 -0400

    Resolução do desafio

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ app.use(express.json());
 
 const repositories = [];
 
+// A rota "get" irá retornar uma lista contendo todos os repositórios cadastrados.
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
+// A rota "post" irá criar um repositório e retornar um objeto com as informações do repositório criado.
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
 
@@ -23,32 +25,36 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
+// A rota "put" irá alterar apenas as informações recebidas pelo corpo da requisição e retornar um repositório atualizado.
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repository = repositories.find((repository) => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
-  }
+  };
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
 
   return response.json(repository);
 });
 
+// A rota delete irá receber o id do repositório que será excluído e retornar um status "204" após a exclusão.
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex === -1) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -57,18 +63,19 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
+// A rota post irá incrementar a quantidade de likes em 1 a cada chamada na rota post e será retornado o repositório com a quantidade de likes atualizada.
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex === -1) {
     return response.status(404).json({ error: "Repository not found" });
-  }
+  };
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes++;
 
-  return response.json('likes');
+  return response.status(201).json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
